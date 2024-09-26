761a7ffd17c9ecbc577b0526315dc07b63a41451
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-09-26 13:09:58 +0530

    Update PeEnquiryQueryBuilder.java

--- business-services/ebinder-v1/src/main/java/digit/repository/querybuilder/PeEnquiryQueryBuilder.java
+++ business-services/ebinder-v1/src/main/java/digit/repository/querybuilder/PeEnquiryQueryBuilder.java
@@ -13,7 +13,8 @@ public class PeEnquiryQueryBuilder {
 
     private static final String BASE_QUERY = """
 
-            select  epe.newcode, epe.oldcode,to_timestamp(epe.orderdate/1000)::date as orderdate, epe.orderno, epe.cedesig as empDesig, epe.cedept as empDept, epe.ceempcode, epe.enqsubject
+            select  epe.newcode, epe.oldcode,to_timestamp(epe.orderdate/1000)::date as orderdate, epe.orderno, 
+            epe.cedesig as empDesig, epe.cedept as empDept, epe.ceempcode, epe.enqsubject,eed.emplname
             
                  """;
     private static final String ECL_query = """
@@ -25,6 +26,7 @@ public class PeEnquiryQueryBuilder {
     private static final String FROM_TABLES = """
             from eg_ebinder_pe_enquiry epe 
             left join eg_ebinder_ce_list ecl on epe.id = ecl.peenquiryid
+            left join eg_employee_data eed on epe.ceempcode = eed.empcode
 
          """;
 
@@ -55,7 +57,6 @@ public class PeEnquiryQueryBuilder {
         }
     }
 
-    
                  
 
 }
