4a97e961277872229d1e39f3100f6230fae224cb
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-04 15:27:47 +0530

    Update CommonRowMapper.java

--- business-services/deonar-v1/src/main/java/digit/repository/rowmapper/CommonRowMapper.java
+++ business-services/deonar-v1/src/main/java/digit/repository/rowmapper/CommonRowMapper.java
@@ -8,9 +8,14 @@ import java.util.List;
 import java.util.Map;
 
 import digit.web.models.security.AnimalType;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
+import java.util.Set;
+import java.util.HashSet;
+
+import java.sql.ResultSetMetaData;
 
 import digit.web.models.common.CommonDetails;
 @Component
@@ -18,6 +23,11 @@ public class CommonRowMapper implements ResultSetExtractor<List<CommonDetails>>{
     @Override
     public List<CommonDetails> extractData(ResultSet rs) throws SQLException, DataAccessException {
         Map<Long, CommonDetails> commonDetailsMap = new LinkedHashMap<>();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        Set<String> columns = new HashSet<>();
+        for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+            columns.add(rsmd.getColumnName(i).toLowerCase());
+        }
         while (rs.next()) {
             Long commonID = rs.getLong("id");
             CommonDetails commonDetails = commonDetailsMap.get(commonID);
@@ -25,18 +35,30 @@ public class CommonRowMapper implements ResultSetExtractor<List<CommonDetails>>{
                 commonDetails = CommonDetails.builder()
                         .name(rs.getString("name"))
                         .id(rs.getLong("id"))         
-                        .licenceNumber(rs.getString("licencenumber")) 
+                       
                         .animalType(new ArrayList<>())
-                        .mobileNumber(rs.getString("mobilenumber"))
-                        .tradertype(rs.getString("tradertype"))
-                        .registrationnumber(rs.getString("registrationnumber"))
+                        
                         .build();
                     }
-                commonDetailsMap.put(commonID, commonDetails);
+            if(columns.contains("licencenumber")){
+                commonDetails.setLicenceNumber(rs.getString("licencenumber"));
+            }
+            if(columns.contains("mobilenumber")){
+                commonDetails.setMobileNumber(rs.getString("mobilenumber"));
+            }
+            if(columns.contains("tradertype")){
+                commonDetails.setTradertype(rs.getString("tradertype"));
+            }
+            if(columns.contains("registrationnumber")){
+                commonDetails.setRegistrationnumber(rs.getString("registrationnumber"));
+            }
+            commonDetailsMap.put(commonID, commonDetails);
+            if(columns.contains("animalId")){
+
                 AnimalType animalType = AnimalType.builder().id(rs.getInt("animalId"))  
                         .name(rs.getString("animalType")).build(); 
                 commonDetails.getAnimalType().add(animalType);
-           
+            }
         }
 
         return new ArrayList<>(commonDetailsMap.values());
