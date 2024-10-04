d9d6589f112019fcf3276dc928b467b9b4333765
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-04 15:26:33 +0530

    Update CommonDetails.java

--- business-services/deonar-v1/src/main/java/digit/web/models/common/CommonDetails.java
+++ business-services/deonar-v1/src/main/java/digit/web/models/common/CommonDetails.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
