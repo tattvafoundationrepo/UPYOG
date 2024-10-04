6ffce854b250397efba16b0ebe573aab3d2544f0
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-04 11:37:03 +0530

    Update CommonQueryBuilder.java

--- business-services/deonar-v1/src/main/java/digit/repository/querybuilder/CommonQueryBuilder.java
+++ business-services/deonar-v1/src/main/java/digit/repository/querybuilder/CommonQueryBuilder.java
@@ -2,6 +2,7 @@ package digit.repository.querybuilder;
 
 import java.util.List;
 
+
 import org.springframework.stereotype.Component;
 
 import digit.repository.CommonSearchCriteria;
@@ -18,24 +19,68 @@ public class CommonQueryBuilder {
     private static final String ORDERBY_NAME = " ORDER BY name DESC ";
 
     private static final String STAKEHOLDER_BASE_QUERY = """
-            SELECT \
-            SK.id , SK.stakeholdername as name \
-            from eg_deonar_stakeholder SK \
-            left join eg_deonar_stakeholder_type_mapping SKTM on SKTM.stakeholderid = SK.id \
-            left join eg_deonar_stakeholders_type SKT on SKT.id = stakeholdertypeid \
-            where lower(SKT.name) = ? \
-            order by SK.stakeholdername
+              SELECT \
+               SK.id, \
+               SK.stakeholdername AS name,\
+               l.licencenumber as licencenumber,\
+               at.id as animalId,\
+               at.name as animalType \
+                FROM \
+               eg_deonar_stakeholder SK \
+            LEFT JOIN \
+            eg_deonar_stakeholder_type_mapping SKTM ON SKTM.stakeholderid = SK.id \
+            LEFT JOIN \
+            eg_deonar_stakeholders_type SKT ON SKT.id = stakeholdertypeid \
+            LEFT JOIN \
+            eg_deonar_stakeholder_licence_mapping sl ON sl.stakeholderid = SK.id \
+            LEFT JOIN \
+            eg_deonar_licence l ON l.id = sl.licenceid \
+            LEFT JOIN \
+            eg_deonar_stakeholder_animal_type_mapping sam ON sam.stakeholdertypeid = SKT.id \
+            LEFT JOIN \
+            eg_deonar_animal_type at ON at.id = sam.animalTypeId \
+            where  lower( SKT.name) = ? \
+            ORDER BY \
+            SK.stakeholdername
+              """;
+
+    private static final String STAKEHOLDER_VIEW_QUERY = """
+            select stakeholdername as name , stakeholderid as id, licencenumber, animalid as animalId , animaltype as animalType
             """;
 
-    public String getStakeHolderQuery(CommonSearchCriteria criteria, List<Object> preparedStmtList){
-        StringBuilder query = new StringBuilder(STAKEHOLDER_BASE_QUERY);
-        preparedStmtList.add(criteria.getOption().toLowerCase());
+    public String getStakeHolderQuery(CommonSearchCriteria criteria, List<Object> preparedStmtList) {
+        StringBuilder query = new StringBuilder(STAKEHOLDER_VIEW_QUERY);
+        String stakeHolder = criteria.getOption().toLowerCase();
+        switch (stakeHolder) {
+            case "trader":
+                query.append(" from eg_deonar_vtrader ");
+
+                break;
+            case "shopkeeper":
+                query.append(" from eg_deonar_vshopkeeper ");
+                break;
+            case "gawal":
+                query.append(" from eg_deonar_vgawal ");
+
+                break;
+            case "dawanwala":
+                query.append(" from  eg_deonar_vdawanwala ");
+
+                break;
+            case "helkari":
+                query.append(" from eg_deonar_vhelkari ");
+
+                break;
+
+            default:
+                break;
+        }
         return query.toString();
     }
-    
+
     public String getSchemeSearchQuery(CommonSearchCriteria criteria, List<Object> preparedStmtList) {
         StringBuilder query = new StringBuilder(BASE_QUERY);
-        
+
         switch (criteria.getOption().toLowerCase()) {
             case "animal":
                 query.append("eg_deonar_animal_type as tbl ");
