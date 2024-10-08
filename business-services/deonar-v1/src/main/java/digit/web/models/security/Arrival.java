83f93bdb279585d6ebe311fc2cdc154b3106467d
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-08 09:21:49 +0530

    Update Arrival.java

--- business-services/deonar-v1/src/main/java/digit/web/models/security/Arrival.java
+++ business-services/deonar-v1/src/main/java/digit/web/models/security/Arrival.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import java.sql.Date;
 import java.sql.Time;
 
+import digit.web.models.inspection.Properties;
+
 @Data
 @Builder
 public class Arrival {
@@ -20,4 +22,6 @@ public class Arrival {
     private Long updatedAt;
     private Integer createdBy;
     private Integer updatedBy;
+    private Properties receiptMode;
+    private String paymentReferenceNumber;
 }
