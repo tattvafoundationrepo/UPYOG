432ed5318b7d52c35629cadd688c836bcb46a395
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-08 12:31:07 +0530

    Update SecurityCheckQueryBuilder.java

--- business-services/deonar-v1/src/main/java/digit/repository/querybuilder/SecurityCheckQueryBuilder.java
+++ business-services/deonar-v1/src/main/java/digit/repository/querybuilder/SecurityCheckQueryBuilder.java
@@ -8,31 +8,48 @@ import digit.web.models.security.SecurityCheckCriteria;
 public class SecurityCheckQueryBuilder {
 
     private static final String BASE_QUERY = """
-        select \
-        i.arrivalid  \
-        ,i.importpermission  \
-        ,a.stakeholdername  \
-        ,TO_TIMESTAMP(i.dateofarrival / 1000)::date as dateofarrival \
-        ,i.timeofarrival::time as timeofarrival  \
-        ,i.vehiclenumber  \
-        ,a.mobilenumber  \
-        ,a.email  \
-        ,c.name as stakeholdertypename  \
-        ,e.licencenumber  \
-        ,e.registrationnumber  \
-        ,TO_TIMESTAMP(dateofarrival / 1000)::date as validtodate  \
-        ,h.name as animaltype  \
-        ,j.count  \
-        from public.eg_deonar_arrival i   \
-        left join public.eg_deonar_stakeholder a on  a.id = i.stakeholderid  \
-        left join public.eg_deonar_stakeholder_type_mapping b on a.id = b.stakeholderid  \
-        left join public.eg_deonar_stakeholders_type c on c.id = b.stakeholdertypeid  \
-        left join public.eg_deonar_stakeholder_licence_mapping d on a.id = d.stakeholderid  \
-        left join public.eg_deonar_licence e on e.id = d.licenceid  \
-        left join public.eg_deonar_stakeholder_animal_type_mapping f on c.id = f.stakeholdertypeid  \
-        left join public.eg_deonar_animal_at_arrival j on i.id = j.arrivalid  \
-        left join public.eg_deonar_animal_type h on h.id = j.animaltypeid  \
-    """;
+                select \
+                i.arrivalid  \
+                ,i.importpermission  \
+                ,a.stakeholdername  \
+                ,TO_TIMESTAMP(i.dateofarrival / 1000)::date as dateofarrival \
+                ,i.timeofarrival::time as timeofarrival  \
+                ,i.vehiclenumber  \
+                ,a.mobilenumber  \
+                ,a.email  \
+                ,c.name as stakeholdertypename  \
+                ,e.licencenumber  \
+                ,e.registrationnumber  \
+                ,TO_TIMESTAMP(dateofarrival / 1000)::date as validtodate  \
+                ,h.name as animaltype  \
+                ,j.count  \
+                from public.eg_deonar_arrival i   \
+                left join public.eg_deonar_stakeholder a on  a.id = i.stakeholderid  \
+                left join public.eg_deonar_stakeholder_type_mapping b on a.id = b.stakeholderid  \
+                left join public.eg_deonar_stakeholders_type c on c.id = b.stakeholdertypeid  \
+                left join public.eg_deonar_stakeholder_licence_mapping d on a.id = d.stakeholderid  \
+                left join public.eg_deonar_licence e on e.id = d.licenceid  \
+                left join public.eg_deonar_stakeholder_animal_type_mapping f on c.id = f.stakeholdertypeid  \
+                left join public.eg_deonar_animal_at_arrival j on i.id = j.arrivalid  \
+                left join public.eg_deonar_animal_type h on h.id = j.animaltypeid  \
+            """;
+    private static final String BASE_QUERY2 = """
+                                select   i.arrivalid      ,i.importpermission      ,a.stakeholdername      ,TO_TIMESTAMP(i.dateofarrival / 1000)::date as dateofarrival
+             ,i.timeofarrival::time as timeofarrival      ,i.vehiclenumber      ,a.mobilenumber      ,a.email      ,c.name as stakeholdertypename
+             ,e.licencenumber      ,e.registrationnumber      ,TO_TIMESTAMP(dateofarrival / 1000)::date as validtodate      , h.name as animaltype
+             ,j.count
+            from public.eg_deonar_arrival i
+             left join public.eg_deonar_stakeholder a on  a.id = i.stakeholderid
+             left join public.eg_deonar_stakeholder_type_mapping b on a.id = b.stakeholderid
+             left join public.eg_deonar_stakeholders_type c on c.id = b.stakeholdertypeid
+             left join public.eg_deonar_stakeholder_licence_mapping d on a.id = d.stakeholderid
+             left join public.eg_deonar_licence e on e.id = d.licenceid
+             left join public.eg_deonar_stakeholder_animal_type_mapping f on c.id = f.stakeholdertypeid
+             left join public.eg_deonar_animal_type h on h.id = f.animaltypeid
+             left join public.eg_deonar_animal_at_arrival j on i.id = j.arrivalid and j.animaltypeid = f.animaltypeid
+             where count is not null
+
+                                """;
 
     private void addClauseIfRequired(StringBuilder query, List<Object> preparedStmtList) {
         if (preparedStmtList.isEmpty()) {
@@ -58,7 +75,7 @@ public class SecurityCheckQueryBuilder {
     }
 
     public String getSearchQuery(SecurityCheckCriteria criteria, List<Object> preparedStmtList) {
-        StringBuilder query = new StringBuilder(BASE_QUERY);
+        StringBuilder query = new StringBuilder(BASE_QUERY2);
 
         if (criteria.getArrivalUuid() != null) {
             addClauseIfRequired(query, preparedStmtList);
