b670ed2856c48a39a9c58be4dd2b72a074f69f4d
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-08 09:21:21 +0530

    Update AnimalAtArrival.java

--- business-services/deonar-v1/src/main/java/digit/web/models/security/AnimalAtArrival.java
+++ business-services/deonar-v1/src/main/java/digit/web/models/security/AnimalAtArrival.java
@@ -2,6 +2,7 @@ package digit.web.models.security;
 
 import lombok.Builder;
 import lombok.Data;
+import java.util.List;
 
 @Data
 @Builder
@@ -9,7 +10,8 @@ public class AnimalAtArrival {
     private Long id;
     private Long arrivalId;
     private Integer animalTypeId;
-    private Integer count;
+    private List<Integer> count;
+    private Integer tokenNum;
     private Long createdAt;
     private Long updatedAt;
     private Integer createdBy;
