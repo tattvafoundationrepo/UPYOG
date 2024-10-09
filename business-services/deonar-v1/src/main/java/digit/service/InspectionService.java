bb709560165da1a390e011867154566ddbbf4a4d
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-09 14:18:52 +0530

    Update InspectionService.java

--- business-services/deonar-v1/src/main/java/digit/service/InspectionService.java
+++ business-services/deonar-v1/src/main/java/digit/service/InspectionService.java
@@ -49,19 +49,6 @@ public class InspectionService {
         InspectionSearchRequest request = new InspectionSearchRequest();
         ArrivalDetailsResponse ere= repository.getArrivalDetails(inspectionRequest.getArrivalId());
 
-        // Animal animal=new Animal();
-        // for (int animalTypeId: ere.getAnimalTyeId())
-        // {
-        //     animal.setArrivalId(ere.getAId());
-        //     animal.setAnimalType(animalTypeId);
-        //     animal.setUpdatedBy(username);
-        //     animal.setCreatedBy(username);
-        //     animal.setAnimalTokenNum(inspectionRequest.getAnimalTokenNumber().getName());
-        //     animal.setCreatedAt(time);
-        //     animal.setUpdatedAt(time);
-
-        // }
-
         Inspection inspection=new Inspection();
         inspection.setArrivalId(ere.getAId());
         inspection.setEmployeeId(inspectionRequest.getRequestInfo().getUserInfo().getUuid());
@@ -74,7 +61,6 @@ public class InspectionService {
         inspection.setUpdatedBy(username);
         inspection.setInspectionType(1);
 
-      //  List<InspectionDetail> list = populateListOfInspectionDetail(inspectionRequest);
 
         List<InspectionIndicators> details = populateListOfInspectionDetails(inspectionRequest);
         Gson gson = new Gson();
@@ -86,10 +72,10 @@ public class InspectionService {
         inspectionDetail.setUpdatedAt(time);
         inspectionDetail.setUpdatedBy(username);
         inspectionDetail.setResultRemark(inspectionRequest.getRemark());
+        inspectionDetail.setAnimalTypeId(inspectionRequest.getAnimalTypeId());
 
         request.setInspectionRequest(inspectionRequest);
         request.setInspectionDetail(inspectionDetail);
-      //  request.setAnimal(animal);
         request.setInspection(inspection);
 
         if (inspectionRequest.getInspectionType().equalsIgnoreCase("Ante Mortem Inspection") || inspectionRequest.getInspectionType().equalsIgnoreCase("Re-Ante Mortem Inspection")) {
@@ -99,31 +85,6 @@ public class InspectionService {
         return inspectionRequest;
 
     }
-
-    // private List<InspectionDetail> populateListOfInspectionDetail(InspectionRequest inspectionRequest) {
-    //     List<InspectionDetail> list = new ArrayList<>();
-    //     Long time = System.currentTimeMillis();
-    //     String username = inspectionRequest.getRequestInfo().getUserInfo().getUserName();
-    //     list.add(new InspectionDetail(IndicatorName.PULSE_RATE.getId(), inspectionRequest.getPulseRate().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.EYES.getId(), inspectionRequest.getEyes().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.SPECIES.getId(), inspectionRequest.getSpecies().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.BREED.getId(), inspectionRequest.getBreed().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.PREGNANCY.getId(), inspectionRequest.getPregnancy().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.POSTURE.getId(), inspectionRequest.getPosture().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.BODY_TEMPERATURE.getId(), inspectionRequest.getBodyTemperature().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.BODY_COLOR.getId(), inspectionRequest.getBodyColor().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.APPROXIMATE_AGE.getId(), inspectionRequest.getApproximateAge().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.APPETITE.getId(), inspectionRequest.getAppetite().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.OPINION.getId(), inspectionRequest.getOpinion().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.GAIT.getId(), inspectionRequest.getGait().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.NOSTRILS.getId(), inspectionRequest.getNostrils().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.MUZZLE.getId(), inspectionRequest.getMuzzle().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.SEX.getId(), inspectionRequest.getSex().getName(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.OTHER.getId(), inspectionRequest.getOther(), time, time, username, username));
-    //     list.add(new InspectionDetail(IndicatorName.REMARK.getId(), inspectionRequest.getRemark(), time, time, username, username));
-
-    //     return list;
-    // }
 
     private List<InspectionIndicators> populateListOfInspectionDetails(InspectionRequest inspectionRequest) {
         List<InspectionIndicators> list = new ArrayList<>();
