2bf6bab6565282bc7c95f0edbb7b471bae3ef5cd
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-10 17:06:43 +0530

    Update IndicatorName.java

--- business-services/deonar-v1/src/main/java/digit/web/models/inspection/IndicatorName.java
+++ business-services/deonar-v1/src/main/java/digit/web/models/inspection/IndicatorName.java
@@ -11,28 +11,29 @@ import java.util.Objects;
 
 
 public enum IndicatorName {
-    PULSE_RATE(1L),
-    EYES(2L),
-    SPECIES(3L),
-    BREED(4L),
-    SEX(5L),
-    BODY_COLOR(6L),
-    PREGNANCY(7L),
-    APPROXIMATE_AGE(8L),
-    GAIT(9L),
-    POSTURE(10L),
-    BODY_TEMPERATURE(11L),
-    APPETITE(12L),
-    NOSTRILS(13L),
-    MUZZLE(14L),
-    OPINION(15L),
-    OTHER(16L),
-    REMARK(17L);
+    PULSE_RATE("Pulse Rate"),
+    EYES("Eyes"),
+    SPECIES("Species"),
+    BREED("Breed"),
+    SEX("Sex"),
+    BODY_COLOR("Body Color"),
+    PREGNANCY("Pregnancy"),
+    APPROXIMATE_AGE("Approximate Age"),
+    GAIT("Gait"),
+    POSTURE("Posture"),
+    BODY_TEMPERATURE("Body Temperature"),
+    APPETITE("Appetite"),
+    NOSTRILS("Nostrils"),
+    MUZZLE("Muzzle"),
+    OPINION("Opinion"),
+    OTHER("Other"),
+    REMARK("Remark");
 
-    private final Long id;
-    public static IndicatorName fromId(Long value) {
+    private final String value;
+
+    public static IndicatorName fromValue(String value) {
         for (IndicatorName name : IndicatorName.values()) {
-            if(Objects.equals(name.getId(), value)) {
+            if (Objects.equals(name.getValue(), value)) {
                 return name;
             }
         }
