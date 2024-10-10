01c1717e9b373ddd2ac0851b0ff157d60c98a3bc
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-10 17:07:39 +0530

    Update InspectionDetails.java

--- business-services/deonar-v1/src/main/java/digit/web/models/inspection/InspectionDetails.java
+++ business-services/deonar-v1/src/main/java/digit/web/models/inspection/InspectionDetails.java
@@ -23,37 +23,27 @@ public class InspectionDetails {
     @JsonProperty("inspectionType")
     private String inspectionType;
 
-    @JsonProperty("importPermission")
-    private String importPermission;
-
-    @JsonProperty("licenceNumber")
-    private String licenceNumber;
-
-    @JsonProperty("traderName")
-    private String traderName;
-
-    @JsonProperty("veterinaryOfficerName")
-    private String veterinaryOfficerName;
-
     @JsonProperty("inspectionDate")
     private String inspectionDate;
 
-    @JsonProperty("inspectionDay")
-    private String inspectionDay;
-
     @JsonProperty("animalDetails")
-    private List<AnimalDetail> animalDetails;
+    private AnimalDetail animalDetails;
 
     @JsonProperty("animalTokenNumber")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private String animalTokenNumber;
 
-    @JsonProperty("indicatorValue")
-    private String indicatorValue;
+    private String resultmark;
 
-//    @JsonProperty("inspectionIndicator")
-//    private InspectionIndicator inspectionIndicator;
+    private Long id;
 
-    @JsonProperty("animalStabling")
-    private String animalStabling;
+    private  List<InspectionIndicators> report;
+
+    private String inspectiontime;
+
+    private Long inspectionid;
+
+
+
 }
+
