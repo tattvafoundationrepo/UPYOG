70b6df79c70b4d76ec7696f4d12d662b1b7c1315
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-14 12:30:31 +0530

    Create AnimalAssignmentService.java

---
+++ business-services/deonar-v1/src/main/java/digit/service/AnimalAssignmentService.java
@@ -0,0 +1,33 @@
+package digit.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import digit.kafka.Producer;
+import digit.web.models.AnimalAssignment;
+import digit.web.models.AnimalAssignmentRequest;
+
+
+@Service
+public class AnimalAssignmentService {
+
+     
+     @Autowired
+     private Producer producer;
+
+     public void assignAnimalsToStakeholder(AnimalAssignmentRequest request) {
+       
+        Long time = System.currentTimeMillis();
+        for(AnimalAssignment animal:request.getAnimalAssignments()){
+            animal.setCreatedAt(time);
+            animal.setCreatedBy(request.getRequestInfo().getUserInfo().getId());
+            animal.setUpdatedAt(time);
+            animal.setUpdatedBy(request.getRequestInfo().getUserInfo().getId());
+        }
+        producer.push("save-animal-assignment", request);
+
+    }
+
+
+}
