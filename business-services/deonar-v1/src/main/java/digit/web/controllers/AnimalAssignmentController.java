d5d4d8306803b4e4f889ec62e1a7889a4ccaae93
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-14 12:31:20 +0530

    Create AnimalAssignmentController.java

---
+++ business-services/deonar-v1/src/main/java/digit/web/controllers/AnimalAssignmentController.java
@@ -0,0 +1,30 @@
+package digit.web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import digit.service.AnimalAssignmentService;
+import digit.web.models.AnimalAssignmentRequest;
+
+@Controller
+public class AnimalAssignmentController {
+    
+    @Autowired
+    AnimalAssignmentService service;
+
+    @PostMapping("/assign/animal")
+    public ResponseEntity<String> aaaignAnimal(@RequestBody AnimalAssignmentRequest request){
+        try {
+            service.assignAnimalsToStakeholder(request);
+            return new ResponseEntity<>("Animal assigned Successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("assignment failed", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
