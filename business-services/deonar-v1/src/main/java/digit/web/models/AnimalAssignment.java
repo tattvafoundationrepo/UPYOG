059cb2afe5f4228634b45c2fd6887a517f6550ae
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-14 12:32:26 +0530

    Create AnimalAssignment.java

---
+++ business-services/deonar-v1/src/main/java/digit/web/models/AnimalAssignment.java
@@ -0,0 +1,21 @@
+package digit.web.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimalAssignment {
+    
+    private Long animalTypeId;
+    private Long token;
+    private Long assignedStakeholder;
+    private Long createdAt;
+    private Long updatedAt;
+    private Long createdBy;
+    private Long updatedBy;
+
+
+}
