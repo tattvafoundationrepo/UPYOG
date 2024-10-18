c434b50b8eb9029f87d8f8af7954340964240aa4
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-18 16:33:04 +0530

    Update InspectionService.java

--- business-services/deonar-v1/src/main/java/digit/service/InspectionService.java
+++ business-services/deonar-v1/src/main/java/digit/service/InspectionService.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-
 @Service
 public class InspectionService {
     @Autowired
@@ -25,13 +24,12 @@ public class InspectionService {
     @Autowired
     private Producer producer;
 
-    public ArrivalDetailsResponse getArrivalResponse(String  arrivalId) {
+    public ArrivalDetailsResponse getArrivalResponse(String arrivalId) {
         if (arrivalId == null) {
             throw new CustomException("INVALID_ARRIVAL_ID", "Search Arrival Id is empty");
         }
         return repository.getArrivalDetails(arrivalId);
     }
-
 
     public InspectionRequest saveInspectionDetails(InspectionRequest inspectionRequest) {
         if (inspectionRequest == null) {
@@ -39,17 +37,17 @@ public class InspectionService {
         }
         Long time = System.currentTimeMillis();
         LocalTime currentTime = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         String username = inspectionRequest.getRequestInfo().getUserInfo().getUserName();
 
         InspectionSearchRequest request = new InspectionSearchRequest();
-        ArrivalDetailsResponse ere= repository.getArrivalDetails(inspectionRequest.getArrivalId());
+        ArrivalDetailsResponse ere = repository.getArrivalDetails(inspectionRequest.getArrivalId());
 
-        Inspection inspection=new Inspection();
+        Inspection inspection = new Inspection();
         inspection.setArrivalId(ere.getAId());
         inspection.setEmployeeId(inspectionRequest.getRequestInfo().getUserInfo().getUuid());
         inspection.setInspectionDate(time);
-        inspection.setInspectionTime(currentTime.format(formatter));
+        inspection.setInspectionTime(LocalTime.now().format(formatter));
         inspection.setInspectionUnitId(1);
         inspection.setCreatedAt(time);
         inspection.setUpdatedAt(time);
@@ -61,11 +59,11 @@ public class InspectionService {
         InspectionDetail inspectionDetail = new InspectionDetail();
         inspectionDetail.setCreatedAt(time);
         inspectionDetail.setCreatedBy(username);
-        if(ObjectUtils.isEmpty(inspectionRequest.getInspectionDetailsJson()) || inspectionRequest.getInspectionDetailsJson() == null){
+        if (ObjectUtils.isEmpty(inspectionRequest.getInspectionDetailsJson())
+                || inspectionRequest.getInspectionDetailsJson() == null) {
             List<InspectionIndicators> details = populateListOfInspectionDetails(inspectionRequest);
             inspectionDetail.setReport(gson.toJson(details));
-        }
-        else{
+        } else {
             inspectionDetail.setReport(inspectionRequest.getInspectionDetailsJson());
         }
         inspectionDetail.setTokenNo(inspectionRequest.getAnimalTokenNumber());
@@ -78,59 +76,90 @@ public class InspectionService {
         request.setInspectionDetail(inspectionDetail);
         request.setInspection(inspection);
 
-         producer.push("save-inspection-details", request);
-        
+        producer.push("save-inspection-details", request);
+
         return inspectionRequest;
 
     }
 
     private List<InspectionIndicators> populateListOfInspectionDetails(InspectionRequest inspectionRequest) {
         List<InspectionIndicators> list = new ArrayList<>();
-        list.add(new InspectionIndicators(IndicatorName.PULSE_RATE.getValue(), inspectionRequest.getPulseRate().getName()));
-        list.add(new InspectionIndicators(IndicatorName.EYES.getValue(), inspectionRequest.getEyes().getName()));
-        list.add(new InspectionIndicators(IndicatorName.SPECIES.getValue(), inspectionRequest.getSpecies().getName()));
-        list.add(new InspectionIndicators(IndicatorName.BREED.getValue(), inspectionRequest.getBreed().getName()));
-        list.add(new InspectionIndicators(IndicatorName.PREGNANCY.getValue(), inspectionRequest.getPregnancy().getName()));
-        list.add(new InspectionIndicators(IndicatorName.POSTURE.getValue(), inspectionRequest.getPosture().getName()));
-        list.add(new InspectionIndicators(IndicatorName.BODY_TEMPERATURE.getValue(), inspectionRequest.getBodyTemperature().getName()));
-        list.add(new InspectionIndicators(IndicatorName.BODY_COLOR.getValue(), inspectionRequest.getBodyColor().getName()));
-        list.add(new InspectionIndicators(IndicatorName.APPROXIMATE_AGE.getValue(), inspectionRequest.getApproximateAge().getName()));
-        list.add(new InspectionIndicators(IndicatorName.APPETITE.getValue(), inspectionRequest.getAppetite().getName()));
-        list.add(new InspectionIndicators(IndicatorName.OPINION.getValue(), inspectionRequest.getOpinion().getName()));
-        list.add(new InspectionIndicators(IndicatorName.GAIT.getValue(), inspectionRequest.getGait().getName()));
-        list.add(new InspectionIndicators(IndicatorName.NOSTRILS.getValue(), inspectionRequest.getNostrils().getName()));
-        list.add(new InspectionIndicators(IndicatorName.MUZZLE.getValue(), inspectionRequest.getMuzzle().getName()));
-        list.add(new InspectionIndicators(IndicatorName.SEX.getValue(), inspectionRequest.getSex().getName()));
+        list.add(new InspectionIndicators(IndicatorName.PULSE_RATE.getValue(), inspectionRequest.getPulseRate()));
+        list.add(new InspectionIndicators(IndicatorName.EYES.getValue(), inspectionRequest.getEyes()));
+        list.add(new InspectionIndicators(IndicatorName.SPECIES.getValue(), inspectionRequest.getSpecies()));
+        list.add(new InspectionIndicators(IndicatorName.BREED.getValue(), inspectionRequest.getBreed()));
+        list.add(new InspectionIndicators(IndicatorName.PREGNANCY.getValue(), inspectionRequest.getPregnancy()));
+        list.add(new InspectionIndicators(IndicatorName.POSTURE.getValue(), inspectionRequest.getPosture()));
+        list.add(new InspectionIndicators(IndicatorName.BODY_TEMPERATURE.getValue(),
+                inspectionRequest.getBodyTemperature()));
+        list.add(new InspectionIndicators(IndicatorName.BODY_COLOR.getValue(), inspectionRequest.getBodyColor()));
+        list.add(new InspectionIndicators(IndicatorName.APPROXIMATE_AGE.getValue(),
+                inspectionRequest.getApproximateAge()));
+        list.add(new InspectionIndicators(IndicatorName.APPETITE.getValue(), inspectionRequest.getAppetite()));
+        list.add(new InspectionIndicators(IndicatorName.OPINION.getValue(), inspectionRequest.getOpinion()));
+        list.add(new InspectionIndicators(IndicatorName.GAIT.getValue(), inspectionRequest.getGait()));
+        list.add(new InspectionIndicators(IndicatorName.NOSTRILS.getValue(), inspectionRequest.getNostrils()));
+        list.add(new InspectionIndicators(IndicatorName.MUZZLE.getValue(), inspectionRequest.getMuzzle()));
+        list.add(new InspectionIndicators(IndicatorName.SEX.getValue(), inspectionRequest.getSex()));
         list.add(new InspectionIndicators(IndicatorName.OTHER.getValue(), inspectionRequest.getOther()));
         list.add(new InspectionIndicators(IndicatorName.REMARK.getValue(), inspectionRequest.getRemark()));
+        if (inspectionRequest.getSlaughterReceiptNumber() != null) {
+            list.add(new InspectionIndicators(IndicatorName.SLAUGHTER_RECEIPT_NUMBER.getValue(),
+                    inspectionRequest.getSlaughterReceiptNumber()));
+        }
+        if (inspectionRequest.getVisibleMucusMembrane() != null) {
+            list.add(new InspectionIndicators(IndicatorName.VISIBLE_MUCUS_MEMBRANE.getValue(),
+                    inspectionRequest.getVisibleMucusMembrane()));
+        }
+        if (inspectionRequest.getThoracicCavity() != null) {
+            list.add(new InspectionIndicators(IndicatorName.THORACIC_CAVITY.getValue(),
+                    inspectionRequest.getThoracicCavity()));
+        }
+        if (inspectionRequest.getAbdominalCavity() != null) {
+            list.add(new InspectionIndicators(IndicatorName.ABDOMINAL_CAVITY.getValue(),
+                    inspectionRequest.getAbdominalCavity()));
+        }
+        if (inspectionRequest.getPelvicCavity() != null) {
+            list.add(new InspectionIndicators(IndicatorName.PELVIC_CAVITY.getValue(),
+                    inspectionRequest.getPelvicCavity()));
+        }
+        if (inspectionRequest.getSpecimenCollection() != null) {
+            list.add(new InspectionIndicators(IndicatorName.SPECIMEN_COLLECTION.getValue(),
+                    inspectionRequest.getSpecimenCollection()));
+        }
+        if (inspectionRequest.getSpecialObservation() != null) {
+            list.add(new InspectionIndicators(IndicatorName.SPECIAL_OBSERVATION.getValue(),
+                    inspectionRequest.getSpecialObservation()));
+        }
 
         return list;
     }
 
-    public  List<InspectionDetails> getInspectionDetails(InspectionSearchCriteria criteria){
-       
-       Long id  = repository.getArrivalId(criteria.getEntryUnitId(),criteria.getInspectionType());
-       if (id!=null){
-        List<InspectionDetails> details = repository.getInspectionDetails(criteria.getEntryUnitId(),criteria.getInspectionType());
+    public List<InspectionDetails> getInspectionDetails(InspectionSearchCriteria criteria) {
+
+        Long id = repository.getArrivalId(criteria.getEntryUnitId(), criteria.getInspectionType());
+        if (id != null) {
+            List<InspectionDetails> details = repository.getInspectionDetails(criteria.getEntryUnitId(),
+                    criteria.getInspectionType());
+            return details;
+        }
+        List<InspectionIndicators> list = repository.getInspectionIndicatorsByType(criteria.getInspectionType());
+        List<Map<String, Long>> tokens = repository.getAnimalTypeCounts(criteria.getEntryUnitId());
+        Gson gson = new Gson();
+        for (Map<String, Long> animal : tokens) {
+            InspectionRequest request = new InspectionRequest();
+            request.setRequestInfo(criteria.getRequestInfo());
+            request.setArrivalId(criteria.getEntryUnitId());
+            request.setInspectionDetailsJson(gson.toJson(list));
+            request.setInspectionType(criteria.getInspectionType());
+            request.setAnimalTokenNumber(animal.get("count"));
+            request.setAnimalTypeId(animal.get("animalTypeId"));
+            request.setRemark("ok");
+            saveInspectionDetails(request);
+        }
+        List<InspectionDetails> details = repository.getInspectionDetails(criteria.getEntryUnitId(),
+                criteria.getInspectionType());
         return details;
-       }
-       List<InspectionIndicators> list = repository.getInspectionIndicatorsByType(criteria.getInspectionType());
-       List<Map<String,Long>> tokens = repository.getAnimalTypeCounts(criteria.getEntryUnitId());
-       Gson gson = new Gson();
-       for(Map<String,Long> animal : tokens){
-       InspectionRequest request = new InspectionRequest();
-       request.setRequestInfo(criteria.getRequestInfo());
-       request.setArrivalId(criteria.getEntryUnitId());
-       request.setInspectionDetailsJson(gson.toJson(list));
-       request.setInspectionType(criteria.getInspectionType());
-       request.setAnimalTokenNumber(animal.get("count"));
-       request.setAnimalTypeId(animal.get("animalTypeId"));
-       request.setRemark("ok");
-       saveInspectionDetails(request);
-       }
-       List<InspectionDetails> details = repository.getInspectionDetails(criteria.getEntryUnitId(),criteria.getInspectionType());
-       return details;
     }
 
-
-}       
+}
