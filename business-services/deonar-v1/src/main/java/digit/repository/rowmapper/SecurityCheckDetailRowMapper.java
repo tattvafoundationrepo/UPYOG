99f96134ec4356d716f7a4ac702d0c43ddfac5bb
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-19 12:24:03 +0530

    Update SecurityCheckDetailRowMapper.java

--- business-services/deonar-v1/src/main/java/digit/repository/rowmapper/SecurityCheckDetailRowMapper.java
+++ business-services/deonar-v1/src/main/java/digit/repository/rowmapper/SecurityCheckDetailRowMapper.java
@@ -49,7 +49,7 @@ public class SecurityCheckDetailRowMapper implements ResultSetExtractor<List<Sec
             AnimalDetail animalDetail = AnimalDetail.builder()
                 .animalTypeId(rs.getLong("animaltypeid"))
                 .animalType(rs.getString("animaltype"))
-                .count(rs.getInt("count"))
+                .count(rs.getInt("token"))
                 .build();
             details.getAnimalDetails().add(animalDetail);
         }
