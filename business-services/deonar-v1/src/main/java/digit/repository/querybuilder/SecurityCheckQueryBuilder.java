45fbc7c74ade599720e72803f4d0fd979e3426c2
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-19 15:09:21 +0530

    Update SecurityCheckQueryBuilder.java

--- business-services/deonar-v1/src/main/java/digit/repository/querybuilder/SecurityCheckQueryBuilder.java
+++ business-services/deonar-v1/src/main/java/digit/repository/querybuilder/SecurityCheckQueryBuilder.java
@@ -7,47 +7,12 @@ import digit.web.models.security.SecurityCheckCriteria;
 @Component
 public class SecurityCheckQueryBuilder {
 
-    private static final String BASE_QUERY = """
-                select \
-                i.arrivalid  \
-                ,i.importpermission  \
-                ,a.stakeholdername  \
-                ,TO_TIMESTAMP(i.dateofarrival / 1000)::date as dateofarrival \
-                ,i.timeofarrival::time as timeofarrival  \
-                ,i.vehiclenumber  \
-                ,a.mobilenumber  \
-                ,a.email  \
-                ,c.name as stakeholdertypename  \
-                ,e.licencenumber  \
-                ,e.registrationnumber  \
-                ,TO_TIMESTAMP(dateofarrival / 1000)::date as validtodate  \
-                ,h.name as animaltype  \
-                ,j.count  \
-                from public.eg_deonar_arrival i   \
-                left join public.eg_deonar_stakeholder a on  a.id = i.stakeholderid  \
-                left join public.eg_deonar_stakeholder_type_mapping b on a.id = b.stakeholderid  \
-                left join public.eg_deonar_stakeholders_type c on c.id = b.stakeholdertypeid  \
-                left join public.eg_deonar_stakeholder_licence_mapping d on a.id = d.stakeholderid  \
-                left join public.eg_deonar_licence e on e.id = d.licenceid  \
-                left join public.eg_deonar_stakeholder_animal_type_mapping f on c.id = f.stakeholdertypeid  \
-                left join public.eg_deonar_animal_at_arrival j on i.id = j.arrivalid  \
-                left join public.eg_deonar_animal_type h on h.id = j.animaltypeid  \
-            """;
     private static final String BASE_QUERY2 = """
-                                select   i.arrivalid      ,i.importpermission      ,a.stakeholdername      ,TO_TIMESTAMP(i.updatedat / 1000)::date as dateofarrival,
-             i.timeofarrival::time as timeofarrival      ,i.vehiclenumber      ,a.mobilenumber      ,a.email      ,c.name as stakeholdertypename,
-             TO_TIMESTAMP(i.permissiondate / 1000)::date as permissiondate,e.licencenumber      ,e.registrationnumber      ,TO_TIMESTAMP(i.updatedat / 1000)::date as validtodate      , h.name as animaltype
-             ,j.animaltypeid ,j.token
-            from public.eg_deonar_arrival i
-             left join public.eg_deonar_stakeholder a on  a.id = i.stakeholderid
-             left join public.eg_deonar_stakeholder_type_mapping b on a.id = b.stakeholderid
-             left join public.eg_deonar_stakeholders_type c on c.id = b.stakeholdertypeid
-             left join public.eg_deonar_stakeholder_licence_mapping d on a.id = d.stakeholderid
-             left join public.eg_deonar_licence e on e.id = d.licenceid
-             left join public.eg_deonar_stakeholder_animal_type_mapping f on c.id = f.stakeholdertypeid
-             left join public.eg_deonar_animal_type h on h.id = f.animaltypeid
-             left join public.eg_deonar_animal_at_arrival j on i.id = j.arrivalid and j.animaltypeid = f.animaltypeid
-             
+            select   arrivalid ,importpermission  ,stakeholdername ,dateofarrival,
+              timeofarrival      ,vehiclenumber      ,mobilenumber      ,email      , stakeholdertypename,
+              permissiondate,licencenumber      ,registrationnumber      , validtodate      ,  animaltype
+             ,animaltypeid ,token
+              from eg_deonar_vmain
 
                                 """;
 
@@ -79,41 +44,41 @@ public class SecurityCheckQueryBuilder {
 
         if (criteria.getArrivalUuid() != null) {
             addClauseIfRequired(query, preparedStmtList);
-            query.append(" i.arrivalid = ? ");
+            query.append(" arrivalid = ? ");
             preparedStmtList.add(criteria.getArrivalUuid());
         }
 
         if (criteria.getImportPermissionNumber() != null) {
             addClauseIfRequired(query, preparedStmtList);
-            query.append(" i.importpermission = ? ");
+            query.append(" importpermission = ? ");
             preparedStmtList.add(criteria.getImportPermissionNumber());
         }
 
         if (criteria.getTraderName() != null) {
             addClauseIfRequired(query, preparedStmtList);
-            query.append(" a.stakeholdername = ? ");
+            query.append(" stakeholdername = ? ");
             preparedStmtList.add(criteria.getTraderName());
         }
 
         if (criteria.getLicenseNumber() != null) {
             addClauseIfRequired(query, preparedStmtList);
-            query.append(" e.licencenumber = ? ");
+            query.append(" licencenumber = ? ");
             preparedStmtList.add(criteria.getLicenseNumber());
         }
 
         if (criteria.getVehicleNumber() != null) {
             addClauseIfRequired(query, preparedStmtList);
-            query.append(" i.vehiclenumber = ? ");
+            query.append(" vehiclenumber = ? ");
             preparedStmtList.add(criteria.getVehicleNumber());
         }
         if(criteria.getTradable() != null){
             addClauseIfRequired(query, preparedStmtList);
-            query.append(" h.tradable = ? ");
+            query.append(" tradable = ? ");
             preparedStmtList.add(criteria.getTradable());
         }
         addClauseIfRequired(query, preparedStmtList);
         query.append(" token is not null ");
-        query.append(" order by i.arrivalid ");
+        query.append(" order by arrivalid ");
 
         return query.toString();
     }
