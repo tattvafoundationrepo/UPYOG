2de56445cb659e56a84b0e851cd202731277f79e
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-19 12:25:48 +0530

    Update AnimalAssignmentService.java

--- business-services/deonar-v1/src/main/java/digit/service/AnimalAssignmentService.java
+++ business-services/deonar-v1/src/main/java/digit/service/AnimalAssignmentService.java
@@ -18,14 +18,13 @@ public class AnimalAssignmentService {
 
      public void assignAnimalsToStakeholder(AnimalAssignmentRequest request) {
        
-        Long time = System.currentTimeMillis();
         for(AnimalAssignment animal:request.getAnimalAssignments()){
+            Long time = System.currentTimeMillis();
             animal.setCreatedAt(time);
             animal.setCreatedBy(request.getRequestInfo().getUserInfo().getId());
             animal.setUpdatedAt(time);
             animal.setUpdatedBy(request.getRequestInfo().getUserInfo().getId());
         }
-          System.out.println("eeeeeeeeeeeeerrrrrrrrrrrrrrrrrrreeeeeeeeeee"+request.toString());
         producer.push("save-animal-assignment", request);
 
     }
