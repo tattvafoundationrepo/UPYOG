2580c50d074bd8adcd17d80d65736992e703c565
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-10-30 11:26:24 +0530

    Update Main.java

--- business-services/bmc-service-v1/src/main/java/digit/Main.java
+++ business-services/bmc-service-v1/src/main/java/digit/Main.java
@@ -1,15 +1,18 @@
 package digit;
 
 
+import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 //@Import({ TracerConfiguration.class })
 @SpringBootApplication
 @ComponentScan(basePackages = { "digit", "digit.web.controllers" , "digit.config"})
 //@EnableJpaRepositories(basePackages = "digit.repository")
 @EntityScan(basePackages = {"digit.web.models", "digit.bmc.model"})
+@Import(TracerConfiguration.class)
 public class Main {
 
 
