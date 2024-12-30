647968a92d8ce9e56663590e0ca4ac428fcc1f6c
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-12-30 18:39:24 +0530

    Update SlaughterListRowMapper.java

--- business-services/deonar-v1/src/main/java/digit/repository/rowmapper/SlaughterListRowMapper.java
+++ business-services/deonar-v1/src/main/java/digit/repository/rowmapper/SlaughterListRowMapper.java
@@ -1,6 +1,5 @@
 package digit.repository.rowmapper;
 
-
 import digit.web.models.SlaughterList;
 import digit.web.models.security.AnimalDetail;
 
@@ -26,7 +25,6 @@ public class SlaughterListRowMapper implements ResultSetExtractor<List<Slaughter
 
         while (rs.next()) {
 
-
             String ddreference = isColumnPresent(rs, "ddreference") ? rs.getString("ddreference") : null;
             String arrivalId = isColumnPresent(rs, "arrivalid") ? rs.getString("arrivalid") : null;
             String uniqueKey = (ddreference != null) ? ddreference : arrivalId;
@@ -35,33 +33,33 @@ public class SlaughterListRowMapper implements ResultSetExtractor<List<Slaughter
             if (shopkeeperDetails == null) {
                 shopkeeperDetails = SlaughterList.builder()
                         .ddReference(ddreference)
-                       // .stakeholderId(rs.getInt("stakeholderid"))
-                        .shopkeeperName(rs.getString("stakeholdername"))
-                        .licenceNumber(rs.getString("licencenumber"))
-                        .mobileNumber(rs.getString("mobilenumber"))
+                        .stakeholderId(isColumnPresent(rs, "stakeholderid") ? rs.getInt("stakeholderid") : null)
+                        .shopkeeperName(isColumnPresent(rs, "stakeholdername") ? rs.getString("stakeholdername") : null)
+                        .licenceNumber(isColumnPresent(rs, "licencenumber") ? rs.getString("licencenumber") : null)
+                        .mobileNumber(isColumnPresent(rs, "mobilenumber") ? rs.getString("mobilenumber") : null)
                         .arrivalId(arrivalId)
-                        .animalAssignmentDetailsList(new ArrayList<>()) 
-                        .purchaseDate(rs.getString("purchasedate"))
-                        .purchaseTime(rs.getString("purchasetime"))
-                       // .opinion(rs.getString("opinion"))
+                        .animalAssignmentDetailsList(new ArrayList<>())
+                        .purchaseDate(isColumnPresent(rs, "purchasedate") ? rs.getString("purchasedate") : null)
+                        .purchaseTime(isColumnPresent(rs, "purchasetime") ? rs.getString("purchasetime") : null)
+                        .opinion(isColumnPresent(rs, "opinion") ? rs.getString("opinion") : null)
                         .build();
 
                 shopkeeperMap.put(uniqueKey, shopkeeperDetails);
             }
 
-             AnimalDetail animalAssignmentDetails = AnimalDetail.builder()
-                     .animalTypeId(rs.getLong("animaltypeid"))
-                     .count(rs.getInt("token"))
-                    // .animalType(rs.getString("animaltype"))
-                     .build();
+            AnimalDetail animalAssignmentDetails = AnimalDetail.builder()
+                    .animalTypeId(isColumnPresent(rs, "animaltypeid") ? rs.getLong("animaltypeid") : null)
+                    .count(isColumnPresent(rs, "token") ? rs.getInt("token") : null)
+                    .animalType(isColumnPresent(rs, "animaltype") ? rs.getString("animaltype") : null)
+                    .build();
 
-             shopkeeperDetails.getAnimalAssignmentDetailsList().add(animalAssignmentDetails);
+            shopkeeperDetails.getAnimalAssignmentDetailsList().add(animalAssignmentDetails);
         }
 
         return new ArrayList<>(shopkeeperMap.values());
     }
 
-     private boolean isColumnPresent(ResultSet rs, String columnName) {
+    private boolean isColumnPresent(ResultSet rs, String columnName) {
         try {
             ResultSetMetaData metaData = rs.getMetaData();
             int columnCount = metaData.getColumnCount();
@@ -71,6 +69,7 @@ public class SlaughterListRowMapper implements ResultSetExtractor<List<Slaughter
                 }
             }
         } catch (SQLException e) {
+          
         }
         return false;
     }
