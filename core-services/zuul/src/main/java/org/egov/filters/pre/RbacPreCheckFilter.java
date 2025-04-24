cf1d14c8a854943a464623868bc2ac849dc21e47
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2025-04-24 15:57:44 +0530

    Update RbacPreCheckFilter.java

--- core-services/zuul/src/main/java/org/egov/filters/pre/RbacPreCheckFilter.java
+++ core-services/zuul/src/main/java/org/egov/filters/pre/RbacPreCheckFilter.java
@@ -45,6 +45,11 @@ public class RbacPreCheckFilter extends ZuulFilter {
 
     @Override
     public Object run() {
+
+         if(getRequestURI().equalsIgnoreCase("/deonar-v1/common/_get")){
+            setShouldDoRbac(true);  
+            return null;
+        }
         if ((openEndpointsWhitelist.contains(getRequestURI())
             || anonymousEndpointsWhitelist.contains(getRequestURI()))) {
             setShouldDoRbac(false);
